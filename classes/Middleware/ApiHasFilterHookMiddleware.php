d66117ce6fce6878243df17726b4033f0b1901e7
王正东 <wangzhengdong@ecmoban.com>
2020-11-20 09:26:11 +0800

    添加API中间件

---
+++ classes/Middleware/ApiHasFilterHookMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Ecjia\App\Api\Middleware;
+
+
+use Closure;
+use Ecjia\Component\ApiServer\Responses\ApiResponse;
+use RC_Hook;
+
+class ApiHasFilterHookMiddleware
+{
+
+    public function handle($request, Closure $next)
+    {
+        $url = $request->get('url');
+
+        if (RC_Hook::has_filter($url)) {
+            $response = response();
+            $response = RC_Hook::apply_filters($url, $response);
+            $response = new ApiResponse($response);
+            return $response;
+        }
+
+        $response = $next($request);
+
+        return $response;
+    }
+
+}
